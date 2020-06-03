41042fe96228673c271e0e5c4e5e85c9deb95319
CameronGleichauf <66345385+CameronGleichauf@users.noreply.github.com>
2020-06-02 22:58:24 -0400

    Update AnimationModelImplTest.java

--- HW5/test/cs3500/shape/AnimationModelImplTest.java
+++ HW5/test/cs3500/shape/AnimationModelImplTest.java
@@ -1,25 +1,44 @@
 package cs3500.shape;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.junit.Assert.*;
 
 public class AnimationModelImplTest {
 
+  AbstractShape circle = new Circle("C", new Posn(0, 0), new Posn(25, 25), 255,
+          0, 0, 50, 50);
+
+
+  AbstractMove changeToBlue = new ColorMoves(circle, 0, 0, 255, 10, 50);
+
+
+  //EXAMPLES OF ANIMATION MODELS
   AnimationModel justCircle = new AnimationModelImpl(
-          new ArrayList<AbstractShape>(Arrays.asList(new Circle("C", new Posn(0, 0), new Posn(25, 25), 255,
-                  0, 0, 50, 50))), new ArrayList<AbstractMove>());
+          new ArrayList<AbstractShape>(Arrays.asList(circle)), new ArrayList<AbstractMove>());
+
+
+  AnimationModel circleChangeColor = new AnimationModelImpl(
+          new ArrayList<AbstractShape>(Arrays.asList(circle)),
+          new ArrayList<AbstractMove>(Arrays.asList(changeToBlue)));
+
+
 
   //test that justCircle getTextSummary works properly initially
   @Test
   public void justCircleTextSummary() {
-    assertEquals("shape C Circle\n"
-        + "motion C 1 0 0 50 50 255 0 0", justCircle.getTextAnimation());
+    assertEquals("shape C Circle\n" +
+            "motion C 1 0 0 50 50 255 0 0     -1 0 0 50 50 255 0 0", justCircle.getTextAnimation());
   }
 
-
+  //test the circleChangeColor getTextSummary works
+  @Test
+  public void testCircleChangeColorTextSummary() {
+    assertEquals("shape C Circle\n" +
+            "motion C 1 0 0 50 50 255 0 0     10 0 0 50 50 255 0 0",
+            circleChangeColor.getTextAnimation());
+  }
 }
